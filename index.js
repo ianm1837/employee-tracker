4e829a1e9c6e55d4e56eb11c2ddae2f5edb766b4
ianm1837 <ian@iwm.dev>
2023-01-26 14:43:10 -0800

    create inquirer logic and link to external functions

--- index.js
+++ index.js
@@ -1,64 +1,50 @@
-// INQUIERER load menu with the following options
+const inquirer = require('inquirer');
+const addDepartment = require('./src/add/addDepartment');
+const addEmployee = require('./src/add/addEmployee');
+const addRole = require('./src/add/addRole');
+const getAllDepartments = require('./src/get/getAllDepartments');
+const getAllEmployees = require('./src/get/getAllEmployees');
+const getAllRoles = require('./src/get/getAllRoles');
+const updateEmployeeManager = require('./src/update/updateEmployeeManager');
+const updateEmployeeRole = require('./src/update/updateEmployeeRole');
 
-//  view all departments
-//  view all roles
-//  view all employees
-//  add a department
-//  add a role
-//  add an employee
-//  update an employee role
+async function main() {
+  let answer = inquirer.createPromptModule({
+    type: 'list',
+    message: 'what would you like to do?',
+    choices: [
+      'Get all Departments',
+      'Get All Employees',
+      'Get All Roles',
+      'Add Department',
+      'Add Role',
+      'Add Employee',
+      'Update Employee Manager',
+      'Update Employee Role',
+      'Quit',
+    ],
+  });
 
+  if (answer == 'Get all Departments') {
+    await getAllDepartments;
+  } else if (answer == 'Get All Employees') {
+    await getAllEmployees;
+  } else if (answer == 'Get All Roles') {
+    await getAllRoles;
+  } else if (answer == 'Add Department') {
+    await addDepartment;
+  } else if (answer == 'Add Role') {
+    await addRole;
+  } else if (answer == 'Add Employee') {
+    await addEmployee;
+  } else if (answer == 'Update Employee Manager') {
+    await updateEmployeeManager;
+  } else if (answer == 'Update Employee Role') {
+    await updateEmployeeRole;
+  } else if (answer == 'Quit') {
+    console.log('Bye!');
+    process.exit();
+  }
+}
 
-
-// QUERIES
-// ==========================================================================================
-
-// getAllDepartments
-//   returns table with:
-//   | department names | department id's |
-
-// getAllRoles
-//   returns table with: 
-//   | job title | role id | department | salary |
-
-// getAllEmployees
-//   returns table with: 
-//   | employee id | first name | last name | job title | department | salary | manager |
-
-// ==========================================================================================
-
-// addDepartment
-//   returns prompt:
-//     What is the name of the department you would like to add?
-//     Then adds requested department to database
-//     returns success message
-
-// addRole
-//   returns prompt:
-//     do no run prompt if no departments exist
-//     enter role name (not null)
-//     enter salary (not null)
-//     pick department from list 
-//       validate department exist
-
-// addEmployee
-//   returns prompt:
-//     enter first name (not null)
-//     enter last name (not null)
-//     pick role from list (validate: do not create employee if no roles exist not null)
-//     pick manager from list (if there are any; can be null)
-
-// ==========================================================================================
-
-// updateEmployeeRole
-//   returns prompt:
-//     pick from list of employees
-//     enter employee role id
-
-// updateEmployeeManager
-//   returns prompt:
-//     pick from a list of employees
-//     enter new manager
-//     
-
-// ==========================================================================================
+main();
