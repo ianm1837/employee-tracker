dc8fba3c68360ca1169c7840eedfd6c5c72f3612
ianm1837 <ian@iwm.dev>
2023-01-26 16:07:42 -0800

    squashed bugs in schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -3,24 +3,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
+    department_id INT AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (department_id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    role_id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    employee_id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    manager_id INT,
+    PRIMARY KEY (employee_id),
+    FOREIGN KEY (role_id) REFERENCES role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
