30d801a863f5d13d94068d99e30ebeca4fd08c21
ianm1837 <ian@iwm.dev>
2023-01-29 18:03:06 -0800

    wrote new query for get all employees

--- src/get/getAllEmployees.js
+++ src/get/getAllEmployees.js
@@ -5,15 +5,24 @@ const dbConnection = require('../dbConnection');
 const cTable = require('console.table');
 
 async function getAllEmployees() {
-  const dbResponse = await dbConnection(`SELECT * FROM employee_db.employee;`);
+  const dbResponse = await dbConnection(`
+  SELECT 
+	employee.employee_id AS ID, 
+	employee.first_name AS 'First Name', 
+	employee.last_name AS 'Last Name', 
+	role.title AS 'Job Title', 
+	department.department_name As 'Department', 
+	role.salary AS 'Salary',
+    CONCAT(manager.first_name, ' ', manager.last_name) AS Manager
 
-  //TODO: Get names of employees department
+  FROM employee
 
-  //TODO: Get name of job title (role)
+  JOIN role ON role.role_id=employee.role_id
+  JOIN department ON role.department_id=department.department_id
+  LEFT JOIN employee AS manager ON employee.manager_id=manager.employee_id
 
-  //TODO: Get salary from role
-
-  //TODO: Get Manager name
+  ORDER BY ID
+  `);
 
   console.table('All Employees', [...dbResponse[0]]);
 
